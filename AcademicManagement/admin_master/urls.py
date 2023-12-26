1ee50c11b527f2be78976cbee96b9d313ea92b5d
ddugky-sparx <batch02.ddugky@gmail.com>
2023-12-26 17:58:39 +0530

    set side bar urls

--- AcademicManagement/admin_master/urls.py
+++ AcademicManagement/admin_master/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('departments/', views.department_manage),
-    path('designation/', views.designation_manage),
-    path('qualification/', views.qualification_manage),
-    path('class/', views.class_manage),
-    path('division/', views.division_manage),
-    path('employee-category/', views.employee_category_manage),
+    path('departments/', views.department_manage,name="department"),
+    path('designation/', views.designation_manage,name="designation"),
+    path('qualification/', views.qualification_manage,name="qualification"),
+    path('class/', views.class_manage,name="class"),
+    path('division/', views.division_manage,name="division"),
+    path('employee-category/', views.employee_category_manage,name="employee-category"),
 
 ]
